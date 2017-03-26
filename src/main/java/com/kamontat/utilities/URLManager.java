7dfa503fff5e86a0d82c7d27087ceab99021910e
kamontat <kamontat_c@hotmail.com>
2017-03-26 22:21:25 +0700

    Refactor more and more

---
+++ src/main/java/com/kamontat/utilities/URLManager.java
@@ -0,0 +1,257 @@
+package com.kamontat.utilities;
+
+import com.kamontat.constance.Protocol;
+import com.kamontat.constance.RequestMethod;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+
+/**
+ * URL Utility
+ *
+ * @author kamontat
+ * @version 1.0
+ * @since Wed 08/Mar/2017 - 10:30 PM
+ */
+public class URLManager {
+	/**
+	 * http connection.
+	 */
+	public static final Class<HttpURLConnection> HTTP_CONNECTION = HttpURLConnection.class;
+	/**
+	 * https connection.
+	 */
+	public static final Class<HttpsURLConnection> HTTPS_CONNECTION = HttpsURLConnection.class;
+	/**
+	 * default connection.
+	 */
+	public static final Class<URLConnection> CONNECTION = URLConnection.class;
+	
+	/**
+	 * web protocol
+	 */
+	private Protocol protocol;
+	/**
+	 * web url/link
+	 */
+	private URL url;
+	
+	/**
+	 * get url utility
+	 *
+	 * @param protocol
+	 * 		web protocol
+	 * @param link
+	 * 		web link (without <b>http://</b> or <b>https://</b>)
+	 * @return url utility
+	 * @see URLManager
+	 */
+	public static URLManager getUrl(Protocol protocol, String link) {
+		return new URLManager(protocol, link);
+	}
+	
+	/**
+	 * get url utility
+	 *
+	 * @param url
+	 * 		the url link
+	 * @return url utility
+	 * @see URLManager
+	 */
+	public static URLManager getUrl(URL url) {
+		return new URLManager(url);
+	}
+	
+	/**
+	 * get url utility
+	 *
+	 * @param link
+	 * 		the link
+	 * @return url utility
+	 * @see URLManager
+	 */
+	public static URLManager getUrl(String link) {
+		URL url = null;
+		try {
+			url = new URL(link);
+		} catch (MalformedURLException ignore) {
+		}
+		return new URLManager(url);
+	}
+	
+	/**
+	 * Constructor Private
+	 *
+	 * @param protocol
+	 * 		web protocol
+	 * @param link
+	 * 		web link
+	 */
+	private URLManager(Protocol protocol, String link) {
+		this.protocol = protocol;
+		// add protocol
+		link = (protocol == Protocol.HTTP ? "http://": "https://") + link;
+		
+		try {
+			url = new URL(link);
+		} catch (MalformedURLException ignore) {
+		}
+	}
+	
+	/**
+	 * Constructor Private
+	 *
+	 * @param url
+	 * 		web url
+	 */
+	private URLManager(URL url) {
+		this.protocol = Protocol.getProtocol(url);
+		this.url = url;
+	}
+	
+	public URL getUrl() {
+		return url;
+	}
+	
+	public Protocol getProtocol() {
+		return protocol;
+	}
+	
+	/**
+	 * get connection
+	 *
+	 * @return url connection
+	 * @see URL#openConnection() or {@code null}
+	 */
+	public <T extends URLConnection> T getConnection(Class<T> tClass) {
+		if (checkNull()) return null;
+		try {
+			return tClass.cast(url.openConnection());
+		} catch (IOException | ClassCastException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * get all content inside url
+	 *
+	 * @return content
+	 */
+	public String getContent() {
+		StringBuilder builder = new StringBuilder();
+		if (checkNull()) return builder.toString();
+		
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getConnection(CONNECTION).getInputStream()));
+			String line = "";
+			while ((line = bufferedReader.readLine()) != null) {
+				builder.append(line).append("\n");
+			}
+		} catch (IOException ignored) {
+		}
+		
+		return builder.toString();
+	}
+	
+	/**
+	 * get content length/size
+	 *
+	 * @return size of content, or {@code -1} if have some error
+	 */
+	public long getURLSize() {
+		if (checkNull()) return -1L;
+		return getConnection(CONNECTION).getContentLengthLong();
+	}
+	
+	/**
+	 * get file name <br>
+	 * Example: https://xyz.com/re/as/fe/test.txt <br>
+	 * the return will be "test.txt"
+	 *
+	 * @return file name
+	 */
+	public String getURLFilename() {
+		String filename = url.getPath();
+		int k = filename.lastIndexOf("/");
+		if (k == filename.length() - 1) return "";
+		if (k >= 0) filename = filename.substring(k + 1);
+		return filename;
+	}
+	
+	// <b>Beware</b>: this can call iff connection is {@link Protocol#HTTPS}.
+	
+	/**
+	 * @param method
+	 * 		request method {@link RequestMethod}
+	 * @param input
+	 * 		can be {@code null}
+	 * @param props
+	 * 		request property e.g. headers({@link com.kamontat.constance.Headers})
+	 * @return input stream from specify request, or {@code null} if cause some error
+	 */
+	public InputStream getSpecifyInputFromConnection(RequestMethod method, String input, RequestProp... props) {
+		if (checkNull()) return null;
+		
+		HttpURLConnection c;
+		if (getProtocol() == Protocol.HTTP) {
+			c = getConnection(HTTP_CONNECTION);
+		} else {
+			c = getConnection(HTTPS_CONNECTION);
+		}
+		
+		if (c == null) return null;
+		try {
+			c.setRequestMethod(method.toString());
+			for (RequestProp p : props) {
+				c.setRequestProperty(p.getKey(), p.getValue());
+			}
+			
+			if (input != null) {
+				c.setDoOutput(true);
+				c.getOutputStream().write(input.getBytes(FilesUtil.DEFAULT_ENCODING));
+			}
+			
+			return c.getInputStream();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * get all header fields
+	 *
+	 * @return header in {@link Map} value, or {@code null}
+	 */
+	public Map<String, List<String>> getHeader() {
+		if (checkNull()) return null;
+		return getConnection(CONNECTION).getHeaderFields();
+	}
+	
+	/**
+	 * print all header to (DEBUG TOOL)
+	 */
+	public void printHeader() {
+		if (checkNull()) {
+			System.err.println("cause some null value");
+			return;
+		}
+		Map header = getHeader();
+		for (Object key : header.keySet()) {
+			System.out.printf("%s: %s\n", key, header.get(key));
+		}
+	}
+	
+	private boolean checkNull() {
+		return url == null;
+	}
+}
